70433a7ee0b8dc9bd8a979d85987f4980d12ba81
Per Sandstrom <pg.sandstrom@gmail.com>
2018-01-14 15:10:19 +0100

    Update release script

--- do_complete_deploy.sh
+++ do_complete_deploy.sh
@@ -7,11 +7,11 @@ cd "$(dirname "$0")"
 ./prep_repo_for_deploy.sh
 
 ./backend/build_production.sh
-
+mkdir -p /apps/nopestradamus/backend
 sudo rsync -a ./backend/release/* /apps/nopestradamus/backend
 
 ./frontend//build_production.sh
-
+mkdir -p /apps/nopestradamus/frontend
 sudo rsync -a ./frontend/release/* /apps/nopestradamus/frontend
 
 pm2 restart nopestradamus-backend
