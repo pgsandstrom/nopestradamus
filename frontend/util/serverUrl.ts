1d48037c4f723d9bad67600aa20c8c77cde8e912
Per Sandström <pg.sandstrom@gmail.com>
2020-08-17 16:51:55 +0200

    Throw error if critical env variable is missing

--- frontend/util/serverUrl.ts
+++ frontend/util/serverUrl.ts
@@ -1,6 +1,10 @@
 export default function getServerUrl() {
   if (typeof window === 'undefined') {
-    return `http://localhost:${process.env.BACKEND_PORT!}`
+    if (process.env.BACKEND_PORT === undefined) {
+      throw new Error('BACKEND_PORT is undefined')
+    }
+
+    return `http://localhost:${process.env.BACKEND_PORT}`
   } else {
     const port = location.port ? `:${location.port}` : ''
     return `${window.location.protocol}//${window.location.hostname}${port}`
