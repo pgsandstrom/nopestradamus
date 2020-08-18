ac602a2de6ef463ad5073f4ef809782803a1f674
Per Sandström <pg.sandstrom@gmail.com>
2020-08-18 19:43:43 +0200

    Update db code

--- backend/src/util/cookie.ts
+++ backend/src/util/cookie.ts
@@ -1,19 +1,18 @@
 import { Request } from 'restify'
 
-// eslint-disable-next-line import/prefer-default-export
-export const getCookie = (request: Request, name: string) => {
-  if (request.headers.cookie) {
+export const getCookieValue = (request: Request, name: string): string | undefined => {
+  if (request.headers.cookie !== undefined) {
     let cookieArray
     if (typeof request.headers.cookie === 'string') {
-      const cookieString = request.headers.cookie as string
+      const cookieString = request.headers.cookie
       cookieArray = cookieString.split(';')
     } else {
       cookieArray = request.headers.cookie as string[]
     }
-    const cookie = cookieArray.find(header => header.trim().startsWith(`${name}=`))
-    if (cookie != null) {
+    const cookie = cookieArray.find((header) => header.trim().startsWith(`${name}=`))
+    if (cookie !== undefined) {
       return cookie.substring(cookie.indexOf('=') + 1)
     }
   }
-  return null
+  return undefined
 }
--- backend/src/util/db.ts
+++ backend/src/util/db.ts
@@ -1,11 +1,19 @@
-import { QueryConfig, Pool } from 'pg'
+import { Pool, PoolClient, QueryConfig, QueryResult, types } from 'pg'
 import config from './config'
 
 const databaseConfig = config().database
 
-let dbPool: Pool
+// TODO null returning instead of undefined from database might screw us. Can we transform all null to undefined?
+
+// Force count-function in database to return number instead of string
+// https://github.com/brianc/node-pg-types#use
+types.setTypeParser(20, (val: string) => {
+  return parseInt(val, 10)
+})
+
+let dbPool: Pool | undefined
 const getDbPool = () => {
-  if (dbPool == null) {
+  if (dbPool === undefined) {
     console.log('creating pool')
     dbPool = new Pool(databaseConfig)
   }
@@ -13,15 +21,35 @@ const getDbPool = () => {
 }
 
 // Use this for single query
-// TODO introduce generics here? Or take cooler db stuff from other project
 export const query = (stuff: QueryConfig) => getDbPool().query(stuff)
-export const queryString = (stuff: string) => getDbPool().query(stuff)
+export const queryString = (stuff: string, values?: any[]) => getDbPool().query(stuff, values)
+
+export const querySingle = async <T = any>(stuff: QueryConfig) => {
+  const result: QueryResult = await getDbPool().query(stuff)
+  return getSingle<T>(result)
+}
+
+export const querySingleString = async <T = any>(stuff: string, values?: any[]) => {
+  const result: QueryResult = await getDbPool().query(stuff, values)
+  return getSingle<T>(result)
+}
+
+const getSingle = <T>(result: QueryResult): T | undefined => {
+  if (result.rowCount > 1) {
+    throw new Error(`Unexpected number of rows: ${result.rowCount}`)
+  } else if (result.rowCount === 0) {
+    return undefined
+  } else {
+    return result.rows[0] as T
+  }
+}
 
 // Use this to gain a client for multiple operations, such as transactions
-export const getClient = () =>
+export const getClient = (): Promise<PoolClient> =>
   new Promise((resolve, reject) => {
-    getDbPool().connect((err, client) => {
-      if (err) {
+    getDbPool().connect((err, client: PoolClient) => {
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+      if (err !== undefined) {
         reject(err)
       } else {
         resolve(client)
@@ -29,7 +57,8 @@ export const getClient = () =>
     })
   })
 
-export const SQL = (parts: TemplateStringsArray, ...values: any[]) => ({
+export const SQL = (parts: TemplateStringsArray, ...values: any[]): QueryConfig => ({
+  // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
   text: parts.reduce((prev, curr, i) => prev + '$' + i + curr), // eslint-disable-line prefer-template
   values,
 })
